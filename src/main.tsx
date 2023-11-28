ef2b152abd7181fea5c355163589b0134970e6f4
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-11-28 06:46:52 +0530

    How react works

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// We use React-Dom to render the component tree. inside the element with id 'root'.
+// our 'App' component is wrapped by another component called <React.StrictMode>.
+// This is built-in component in react. It doesn't have a visual representation.
+// It's purpose is to identify the potential problems.

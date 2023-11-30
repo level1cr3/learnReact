2a85152d6c7eaffb88ba7ad1d350179e0bf59af0
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-11-30 06:44:05 +0530

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let cities: string[] = [
     "New York",
@@ -7,15 +9,30 @@ function ListGroup() {
     "Paris",
   ];
 
-  cities = [];
+  // by convention we write handle in front of event handling functions.
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {cities.map((city) => (
-          <li className="list-group-item" key={city}>
+        {cities.map((city, index) => (
+          <li
+            className="list-group-item"
+            key={city}
+            onClick={
+              handleClick // we are passing reference to function instead of calling it. It will get called in runtime.
+
+              //(event) => console.log(event);
+              // event : is 'SyntheticBaseEvent' this is built-in class in react. Because different browsers have different
+              // implementation of event object. So to make it cross-browser react team created this class.
+              // It is a wrapper around native browser event object.
+
+              // here we can pass small arrowFunc. again if it gets big or complex it is better to move it on
+              // separate function.
+            }
+          >
             {city}
           </li>
         ))}

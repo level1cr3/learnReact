976971bdbb4b7613a2b85cc987b6d0759fc340b4
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-12-04 06:28:25 +0530

    remove unessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import ListGroup from "./components/ListGroup";
-
 function App() {
-  let cities: string[] = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  return (
-    <>
-      <ListGroup
-        items={cities}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;

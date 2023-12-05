b87a78340bd6efcbf13aa774f0a2ef561b68bd24
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-12-05 14:38:20 +0530

    UI Library and react icons lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ListGroup from "./components/ListGroup";
 import "./App.css";
+import { BsCalendar2Fill } from "react-icons/bs";
 
 function App() {
   const colors = ["blue", "red", "yellow"];
   return (
     <>
+      <BsCalendar2Fill color="cyan" size={50} />
       <ListGroup
         items={colors}
         heading="colors"

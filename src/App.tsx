25157c42ef61c0de7f269ce1c05305c9a19603d7
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-12-07 05:59:08 +0530

    Understanding the state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
-import Like from "./components/Like";
+import { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
+  // react will store value somewhere in memory most likely like this
+  //[false,true] . so next time react re-renders our component. it is going to look at this array.
+  // grab first value and store it inside the 'isVisible' array.
+  // So react relies on the order of these elements. So it can map these values properly to local variable
+  // in this function.
+  const [isVisible, setVisibility] = useState(false); // we are using state hook to declare
+  const [isApproved, setApproved] = useState(true); //and initialize two boolean variables.
+
+  // these names that we have here just local identifier in this function. react is not aware of them
+
+  let count = 0;
+
+  const handleClick = () => {
+    setVisibility(true);
+    //setName("gayle");
+
+    count++;
+    console.log(isVisible);
+  }; // react will apply all the updates at once. and then will render our component with updated state.
+
   return (
     <>
-      <Like onClick={() => console.log("clicked")} />
+      <Button onClick={handleClick}>Show</Button>
     </>
   );
 }

bc656870b8eed41a5754a5df8e76f6efe744a5df
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-12-15 12:58:47 +0530

    updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import MyMsg from "./MyMsg";
+import Button from "./components/Button";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    //drink.price = 6; // setting like this won't re-render the component.
+    // we should treat this drink object as immutable. similar to how we treat props as immutable.
+
+    // what we need to do is pass the new drink object with updated value that you want.
+    const newDrink = { ...drink, price: drink.price + 1 };
+    setDrink(newDrink);
+  };
   return (
     <div>
-      <MyMsg />
+      {drink.price}
+      <Button onClick={handleClick}>Click me</Button>
     </div>
   );
 }

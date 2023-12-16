47c5a77039f3b758548a55ae7ce7b898b722a864
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-12-16 07:25:48 +0530

    clean unessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,9 @@ import MyMsg from "./MyMsg";
 import Button from "./components/Button";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
-  });
-
-  const handleClick = () => {
-    //drink.price = 6; // setting like this won't re-render the component.
-    // we should treat this drink object as immutable. similar to how we treat props as immutable.
-
-    // what we need to do is pass the new drink object with updated value that you want.
-    const newDrink = { ...drink, price: drink.price + 1 };
-    setDrink(newDrink);
-  };
+  const handleClick = () => {};
   return (
     <div>
-      {drink.price}
       <Button onClick={handleClick}>Click me</Button>
     </div>
   );

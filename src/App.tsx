21f6d65dcf853705e91d529e648a20b98153257e
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-01-16 06:34:36 +0530

    5: manage component state exercise: 3 done

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@ import { useState } from "react";
 import Button from "./components/Button";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "jhon",
-    toppings: ["mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "product 1", quantity: 1 },
+      { id: 2, title: "product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      {pizza.toppings.join(" ")}
+      {cart.items.map((item) => (
+        <span>
+          ID: {item.id},qty: {item.quantity},title: {item.title}
+          <br />
+        </span>
+      ))}
       <Button onClick={handleClick}>Click me</Button>
     </div>
   );

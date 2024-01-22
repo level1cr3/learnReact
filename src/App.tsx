b9beabb1949492f953ef424c8a25e4f12bdd1736
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-01-22 10:37:54 +0530

    impure component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useRef } from "react";
+
 function App() {
-  return <></>;
+  //lets say when app starts we wanna put focus on this input.
+  const ref = useRef<HTMLInputElement>(null);
+
+  // side effect.
+  if (ref.current) ref.current.focus(); // this piece of code have nothing to do with returning jsx markup.
+  // with this piece of code we changing the state of dom.
+  // It is a sideEffect. It is changing something outside of this component.
+  // now our component is no longer an pure component. 
+
+  return (
+    <>
+      <input ref={ref} type="text" className="form-control" />
+    </>
+  );
 }
 
 export default App;

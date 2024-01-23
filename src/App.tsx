cc086444a56fd75daeb943899043eef0e8af015b
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-01-23 19:57:43 +0530

    Effect hook cleanup if required

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import ProductList from "./connect-backend/ProductList";
 
 function App() {
   const [category, setCategory] = useState("");
+
+  const connect = () => console.log("connecting");
+  const disconnect = () => console.log("disconnecting");
+
+  useEffect(() => {
+    document.title = "my app"; // doesn't need cleanup.
+
+    // but if code needs cleanup like disconnecting from the server
+    connect(); // think of connecting to chat server.
+
+    // so function that we pass to effect hook can optionally return a function for cleanup
+    return () => disconnect(); // we use code like this to run the cleanup code.
+
+    // generally our cleanup function undo's whatever our effect was doing.
+    // ex1 : if we are showing modal. our cleanup should hide the modal.
+  });
+
   return (
     <>
       <select

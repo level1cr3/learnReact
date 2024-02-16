8112b841d0b7f0008a6f83059c620e47a31b8546
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-02-16 06:05:14 +0530

    Deleting Data

--- src/App.tsx
+++ src/App.tsx
@@ -42,14 +42,41 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    // there are 2 approaches when deleting user
+    // 1. Optimistic : Update UI first and then call server to save changes. benefit UI will be super fast
+    // 2. Pessimistic : First we call the server and wait for the result. If the call is successful then we update the UI.
+    // Note : so when possible we use optimistic update.
+
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id != user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err: AxiosError) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
 
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
